454fa1150793a412d804a64f9feb17041c7eaea2
David Martinez Marti <desarrollo@gestiweb.com>
2011-12-02 12:25:48 +0100

    Mejoras asistente extensiones

--- enebootools/assembler/database.py
+++ enebootools/assembler/database.py
@@ -2,6 +2,7 @@
 import os
 import os.path
 import sqlite3
+import re
 
 from lxml import etree
 
@@ -172,6 +173,18 @@ def uinput(question):
     import sys
     text= raw_input(unicode(question).encode(sys.stdout.encoding)).decode(sys.stdin.encoding)
     return text
+
+def uinput_mask(question, mask, errortext = None):
+    if errortext is None:
+        errortext = u"El valor '%s' no es válido"
+    while True:
+        text = uinput(question)
+        m1 = re.search(mask,text)
+        if m1:
+            return text,m1
+        else:
+            print errortext % text
+    
     
 def do_save_fullpatch(iface, feat):
     db = init_database()
@@ -182,28 +195,181 @@ def do_save_fullpatch(iface, feat):
     do_build(iface, target = "fullpatch", feat = feat, rebuild = True, dstfolder = patch_folder)
     oi.set_patch_name(feat, patchname)
     
-def select_option(title,question, options, answers, default = None):
+def select_option(title, options, answers, question = None, errortext = None, default = None, accept_invalid = False):
+    if question is None:
+        question = u"Seleccione una opción: "
+    if errortext is None:
+        errortext = u"El valor '%s' no es una opción válida"
     print title
+    answers = answers[:len(options)]
     for answer, option in zip(answers,options):
         print u"    %s) %s" % (answer, option)
     def ask():
         answer = uinput(question).lower()
         if answer == "": answer = default
-        if answer not in answers: return None
-        else: return answer
+        if answer not in answers: 
+            print errortext % answer
+            return None
+        return answer
     answer = None
-    while answer is None: answer = ask()
+    while answer is None: 
+        answer = ask()
+        if answer is None:
+            if accept_invalid:
+                return None, None
     
     return answer, options[answers.index(answer)]
 
 def do_new(iface):
+    letters = list("abcdefghijklmnopqrstuvwxyz123456789")
     db = init_database()
     oi = ObjectIndex(iface)
     oi.analyze_objects()
-    a,o = select_option( 
-            title = u"Qué tipo de funcionalidad va a crear?", 
-            question = u"Seleccione una opción: ", 
-            options = [u"extensión",u"proyecto", u"conjunto de extensiones"],
-            answers = ["ext","prj","set"],
-            )
-    print a,o
+    
+    # SELECCIONAR CARPETA DONDE SE GUARDARA:
+    folders = []
+    for path in cfg.module.featurefolders:
+        if not os.path.exists(path):
+            iface.debug(u"Se ignora directorio inexistente %s" % repr(path))
+            continue
+        folders.append(path)
+    if len(folders) == 0:
+        iface.error("No hay carpetas válidas donde guardar extensiones. Imposible continuar.")
+        return False
+    fpath = None
+    def change_fpath():
+        print
+        if len(folders) == 1:
+            fpath = folders[0]
+            print u"La funcionalidad se guardará en la única carpeta válida: '%s'" % fpath
+        else:
+            a,fpath = select_option( 
+                    title = u"Existen varias carpetas de funcionalidades:", 
+                    question = u"Seleccione en qué carpeta desea crear la nueva funcionalidad: ", 
+                    options = folders,
+                    answers = letters,
+                    )
+        return fpath
+    fpath = change_fpath()
+    
+    ftype_options = [u"extensión",u"proyecto", u"conjunto de extensiones"]
+    ftype_answers = ["ext","prj","set"]
+    ftype_idx = dict(zip(ftype_answers,ftype_options))
+    def change_ftype():
+        print
+        ftype,o = select_option( 
+                title = u"Qué tipo de funcionalidad va a crear?", 
+                question = u"Seleccione una opción: ", 
+                options = ftype_options,
+                answers = ftype_answers,
+                )
+        return ftype
+    ftype = change_ftype()
+    
+    def change_fcode():
+        print
+        t,m = uinput_mask(
+                    question = u"Código para la nueva funcionalidad: ",
+                    mask = r"^([A-Z0-9]\d{3})$", 
+                    errortext = u"ERROR: El valor '%s' debe seguir el formato A999 (A puede ser número).",
+                    )
+        fcode = m.group(0)
+        return fcode
+    fcode = change_fcode()
+    
+    def change_fname():    
+        print
+        t,m = uinput_mask(
+                    question = u"Nombre corto de funcionalidad: ",
+                    mask = r"^([a-z][a-z0-9_]{3,15})$", 
+                    errortext = u"ERROR: El valor '%s' debe tener entre 4 y 16 carácteres, ser minúsculas y tener solo letras y números (empezando siempre por letra)",
+                    )
+        fname = m.group(0)
+        return fname
+    fname = change_fname()
+    
+    def change_fdesc():
+        print
+        fdesc = uinput(u"Descripción de la funcionalidad: ")
+        return fdesc
+    fdesc = change_fdesc()
+    
+    fdep_modules = []
+    fdep_features = []
+    fload_patch = None
+    while True:
+        fdstpath = os.path.join(fpath,"%s%s-%s" % (ftype, fcode, fname))
+        print
+        print u"**** Asistente de creación de nueva funcionalidad ****"
+        print 
+        print u" : Carpeta destino : %s" % fdstpath
+        print u" : Tipo            : %s " % ftype_idx[ftype]
+        print u" : Código y nombre : %s - %s" % (fcode, fname)
+        print u" : Descripción     : %s " % (fdesc)
+        print
+        menu1_options = []
+        menu1_answers = []
+        menu1_options += [ u"Cambiar datos básicos"]
+        menu1_answers += [ "c" ]
+        menu1_options += [ u"Dependencias", u"Importar parche" ]
+        menu1_answers += [ "d"           , "i" ]
+        menu1_options += [ u"Aceptar y crear", u"Cancelar y Salir" ]
+        menu1_answers += [ "a"              , "q" ]
+        a1,o1 = select_option( 
+                title = u"--  Menú de opciones generales --", 
+                question = u"Seleccione una opción: ", 
+                options = menu1_options,
+                answers = menu1_answers,
+                )
+        if a1 == "c":
+            menu2_options = []
+            menu2_answers = []
+            kvs = menu2_answers, menu2_options
+            def agregar_opcion2(kvs, k,v):
+                ks, vs = kvs
+                ks += [ k ]
+                vs += [ v ]
+            agregar_opcion2(kvs, "0", u"Seleccionar una carpeta diferente")
+            agregar_opcion2(kvs, "1", u"Cambiar tipo de funcionalidad")
+            agregar_opcion2(kvs, "2", u"Cambiar código")
+            agregar_opcion2(kvs, "3", u"Cambiar nombre")
+            agregar_opcion2(kvs, "4", u"Cambiar descripción")
+            agregar_opcion2(kvs, "q", u"Finalizar edición")
+            while True:
+                print
+                print u"**** Cambiar datos básicos ****"
+                print 
+                print u" : Carpeta : %s" % fpath
+                print u" : Tipo    : %s (%s) " % (ftype_idx[ftype],ftype)
+                print u" : Código  : %s" % (fcode)
+                print u" : Nombre  : %s" % (fname)
+                print u" : Descr.  : %s " % (fdesc)
+                print
+                a2,o2 = select_option( 
+                        title = u"--  Menú de datos básicos --", 
+                        question = u"Seleccione una opción: ", 
+                        options = menu2_options,
+                        answers = menu2_answers,
+                        )
+                if a2 == "0":
+                    fpath = change_fpath()
+                if a2 == "1":
+                    ftype = change_ftype()
+                if a2 == "2":
+                    fcode = change_fcode()
+                if a2 == "3":
+                    fname = change_fname()
+                if a2 == "4":
+                    fdesc = change_fdesc()
+                if a2 == "q":
+                    break
+             
+            continue
+        if a1 == "a": 
+            print
+            print u"Guardando ... "
+            break
+        if a1 == "q": 
+            print u"Se ha cancelado la operación."
+            break
+        
